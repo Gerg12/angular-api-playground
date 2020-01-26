bce84a5c3ea3941fa2b97d97cca3e1e69c2548e5
Gregatron <thegregoryjohnson@gmail.com>
2020-01-25 19:02:20 -0500

    Setting up routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { CharactersComponent } from './characters/characters.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'archive/:year/:month', component: ArchiveComponent},
+  { path: 'characters', component: CharactersComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
